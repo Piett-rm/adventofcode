e663705cb6d5392eeabc8e95bb9215bd2950712a
Piett-rm <rmpetit24@gmail.com>
2023-12-04 23:06:07 +0100

    2d

--- 2d.py
+++ 2d.py
@@ -1,7 +1,18 @@
+dict = {
+    "red": 0,
+    "bleu": 0,
+    "green": 0
+}
+
 with open("2d.txt", "r") as fichier:
-    lignes = fichier.read()
-    print()
-    print(lignes.split(" "))   
+    for ligne in fichier.read().splitlines():
+        games = ligne.split(":")
+        color_with_numbers = games[1].replace(";",",").split(",")
+        for color_with_number in color_with_numbers:
+            tirage = color_with_number.strip().split(" ")
+            number = tirage[0]
+            color = tirage[1]
+
         # for lettre in ligne:
         #     if lettre.isnumeric():
         #         print(lettre)
