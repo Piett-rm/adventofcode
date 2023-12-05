29153e1db253202c354ac795e20727ef40bc00c0
Remi PETIT <B426VY@login.axa>
2023-12-05 10:44:45 +0100

    2d end

--- 2d.py
+++ 2d.py
@@ -1,20 +1,37 @@
-dict = {
-    "red": 0,
-    "bleu": 0,
-    "green": 0
+maxColor = {
+    "red": 12,
+    "green":13,
+    "blue": 14,
 }
 
+
+tab = set()
+total = 0
 with open("2d.txt", "r") as fichier:
     for ligne in fichier.read().splitlines():
+        power= {
+            "red":0,
+            "green":0,
+            "blue": 0,
+        }
         games = ligne.split(":")
+        numberGame = games[0].split(" ")
         color_with_numbers = games[1].replace(";",",").split(",")
         for color_with_number in color_with_numbers:
             tirage = color_with_number.strip().split(" ")
             number = tirage[0]
             color = tirage[1]
-
-        # for lettre in ligne:
-        #     if lettre.isnumeric():
-        #         print(lettre)
-        #         tab.append(int(lettre))
-      
+            if int(number) > power[color]:
+                power[color] = int(number)
+            # part 1
+            # if int(number) <= maxColor[color]:
+            #     tab.add(int(numberGame[1]))
+            # else:
+            #     if int(numberGame[1]) in tab:
+            #         tab.remove(int(numberGame[1]))
+            #     break
+        power_ligne = power["red"] * power["green"] * power["blue"]
+        total += power_ligne                
+##sum du tab
+# print(sum(tab))
+print(total)
